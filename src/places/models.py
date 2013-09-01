598e1d0ef685e36fd721fb87ecab78b6ac8ff2b3
Erez Segall <erez.segall@gmail.com>
2013-09-01 13:56:35 +0300

    created place model and db

--- src/places/models.py
+++ src/places/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Place(models.Model):
+    vendor_id = models.CharField(max_length=50, unique=True)
+    data = models.TextField()
