9bd3317e8ccd120ed4e82bed28a68e69825657da
yousef Shamshoum <shamshoumyousef@gmail.com>
2013-09-02 13:12:56 +0300

    Working on homepage

--- src/places/views.py
+++ src/places/views.py
@@ -1,8 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from places.models import Place
+from places.models import Place, Placemark
 
 
 def home(request):
     places = Place.objects.all()
-    context ={'places' : places}
+    all = []
+    for place in places:
+        sum = {}
+        sum['id'] = place.vendor_id
+        sum['num_marker'] = Placemark.objects.filter(place= place).count()
+        all.append(sum)
+    context ={'all' : all}
     return render(request, 'home.html', context )
